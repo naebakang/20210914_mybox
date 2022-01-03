b98c4d5a2ab2450e0d4d02a4e92d9bb0837ac9a1
kjw2031 <kjw2031@jongdali.com>
2022-01-03 15:55:52 +0900

    add, using python flask 1

--- 20220102_tutorial_flask/main.py
+++ 20220102_tutorial_flask/main.py
@@ -6,8 +6,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'Hello, world'
+    return '''
+    <h1>dlrjs h1 wpahr</h1>
+    <p>dlrjs p qhsans</p>
+    <a href="https://google.com">google homepage</a>
+    '''
+
+
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name, user_id):
+    return f'Hello, {user_name}({user_id})!'
 
 
 if __name__ == '__main__':
